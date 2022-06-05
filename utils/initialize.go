0703078a95e4c746959a0a6b2dde6109c70e9c73
chirag-ghosh <cghosh828049@gmail.com>
2022-06-05 20:42:37 +0530

    Changed auth token;

--- utils/initialize.go
+++ utils/initialize.go
@@ -2,4 +2,4 @@ package utils
 
 import "github.com/slack-go/slack"
 
-var Api = slack.New("xoxp-2609252556242-2609264879891-3466541747172-bdff3b97bf5364fe4b7805ac27a2ce78")
+var Api = slack.New("xoxp-2609252556242-3591338548263-3607364792754-2659041d4a8e12810ff1ad84e8b24457")
