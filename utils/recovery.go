6b29179eaed815839ab329cc8e36a46da692a288
chirag-ghosh <cghosh828049@gmail.com>
2022-06-08 12:24:21 +0530

    Added stack trace to recover;

--- utils/recovery.go
+++ utils/recovery.go
@@ -1,9 +1,13 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 func RecoverServer() {
 	if r := recover(); r != nil {
 		fmt.Println("Recovered", r)
+		fmt.Println("stacktrace", string(debug.Stack()))
 	}
 }
